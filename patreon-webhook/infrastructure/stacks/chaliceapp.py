7d2a1e62317d14cc747b33b386046442ecf34e1c
svrakiss <59843464+svrakiss@users.noreply.github.com>
2023-02-01 01:24:25 +0000

    traces actually show up now

--- patreon-webhook/infrastructure/stacks/chaliceapp.py
+++ patreon-webhook/infrastructure/stacks/chaliceapp.py
@@ -2,7 +2,6 @@ import os
 
 from aws_cdk import aws_dynamodb as dynamodb
 import aws_cdk.aws_ssm as ssm
-
 try:
     from aws_cdk import core as cdk
 except ImportError:
@@ -20,12 +19,15 @@ class ChaliceApp(cdk.Stack):
     def __init__(self, scope, id, **kwargs):
         super().__init__(scope, id, **kwargs)
         self.dynamodb_table = self._create_ddb_table()
+        role = iam.Role(self,"DeafRole",assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"));
         self.chalice = Chalice(
             self, 'ChaliceApp', source_dir=RUNTIME_SOURCE_DIR,
             stage_config={
                 'environment_variables': {
                     'APP_TABLE_NAME': self.dynamodb_table.table_name
-                }
+                },
+                'iam_role_arn':role.role_arn,
+                'manage_iam_role':False
             },
 
         )
@@ -34,15 +36,33 @@ class ChaliceApp(cdk.Stack):
         api_handler = self.chalice.sam_template.get_resource('APIHandler')
         api_handler.tracing = 'Active'
         self.dynamodb_table.grant_read_write_data(
-            self.chalice.get_role('DefaultRole')
+            role
         )
-        self.chalice.get_role('DefaultRole').attach_inline_policy(
+       
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AWSXRayDaemonWriteAccess")
+        )
+        role.attach_inline_policy(
             iam.Policy(self,'indexPolicy',
             statements=[iam.PolicyStatement(
                 actions=["dynamodb:Query"],
                 resources=[self.dynamodb_table.table_arn+'/index/*']
             )])
         )
+        role.attach_inline_policy(
+            iam.Policy(self,
+           "logsPolicy",
+            statements=[
+                iam.PolicyStatement(
+                    actions=["logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"],
+                    resources=["arn:*:logs:*:*:*"],
+                    effect=iam.Effect.ALLOW
+                )
+            ])
+        )
+        
 
     def _create_ddb_table(self):
         tableName = ssm.StringParameter.value_for_string_parameter(self,parameter_name="/config/ValidatorMS-production/spring.cloud.aws.dynamodb.tableName")
