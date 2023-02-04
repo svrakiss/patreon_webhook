633aab1d3b73468de1357364294e91c9664d6343
svrakiss <59843464+svrakiss@users.noreply.github.com>
2023-02-04 06:10:38 +0000

    add pynamo

--- patreon-webhook/runtime/app.py
+++ patreon-webhook/runtime/app.py
@@ -2,14 +2,16 @@ import os
 import boto3
 from chalice import Chalice, Response
 from patreon.jsonapi.parser import JSONAPIParser, JSONAPIResource
-import hmac
+from chalicelib.table import Patron 
 from datetime import datetime
 import boto3.dynamodb.types
 from boto3.dynamodb.conditions import Key, Attr
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 from chalice.app import DynamoDBEvent,DynamoDBRecord
+
 patch_all()
+
 
 app = Chalice(app_name='patreon-webhook-webhook')
 dynamodb = boto3.resource('dynamodb')
@@ -19,14 +21,25 @@ app.debug = True
 def find_all(event: DynamoDBEvent):
     for r in event:
         if r.event_name != "MODIFY" or r.keys.get('SortKey') != {"S":"INFO"}: # put this in the filter criteria
+            # "SortKey": ["INFO"]
             continue
         result = decide(r)
-        if result == "disapprove" or result == "approve":
+        if result == "approve": # calculate PatStats
             app.log.debug(f"New {r.new_image}")
-            # dynamodb_table.query(
-                # KeyConditionExpression=Key('PartKey').eq(r.keys.get('PartKey').get("S")).__and__(Key('SortKey').eq('INFO').__invert__())
-            # )
+            to_change = dynamodb_table.query(
+                KeyConditionExpression=Key('PartKey').eq(r.keys.get('PartKey').get("S")).__and__(Key('SortKey').begins_with('CHANNEL'))
+            ) # this can be a lot...
+            to_change_actually =Patron.query(r.keys.get('PartKey').get('S'),Patron.sort_key.startswith("CHANNEL"))
+            app.log.debug(to_change)
+            app.log.debug(to_change_actually)
+        else:
+            print("BNBNB")
+            # remove PatStats
 
+# dynamodb_table.query(IndexName='Category',
+#     KeyConditionExpression=Key('Category').eq(arg),
+#     FilterExpression=Attr('PatStats').exists()
+# )
 def decide(resp:DynamoDBRecord):
     if resp.old_image.get('Status').get('S') == 'active_patron':
         if resp.new_image.get('Status').get('S') != 'active_patron':
